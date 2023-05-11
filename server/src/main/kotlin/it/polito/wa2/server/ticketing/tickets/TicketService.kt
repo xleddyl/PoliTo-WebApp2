234f37a6ce0978e60493e5e091608d195feb5cbc
Edoardo Alberti <edoalberti99@gmail.com>
2023-05-11 15:16:24 +0200

    Changes by Davide

--- server/src/main/kotlin/it/polito/wa2/server/ticketing/tickets/TicketService.kt
+++ server/src/main/kotlin/it/polito/wa2/server/ticketing/tickets/TicketService.kt
@@ -15,4 +15,7 @@ interface TicketService {
 
     @Throws(NotFoundException::class)
     fun editTicket(ticketId: Long, ticketDTO: TicketDTO): TicketDTO
+
+    @Throws(NotFoundException::class, NotValidException::class)
+    fun updateStatus(ticketId: Long, state: States): TicketDTO
 }
