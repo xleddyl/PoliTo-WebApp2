df24a6f4f945427e820837d30a86b7226af8d726
Paolo Vacchetto s305386 <s305386@studenti.polito.it>
2023-05-11 14:33:04 +0200

    Pushed test for ProductsTests

--- server/src/test/kotlin/it/polito/wa2/server/products/ProductsTests.kt
+++ server/src/test/kotlin/it/polito/wa2/server/products/ProductsTests.kt
@@ -3,16 +3,38 @@ package it.polito.wa2.server.products
 import it.polito.wa2.server.AbstractApplicationTest
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
+import org.springframework.core.ParameterizedTypeReference
+import org.springframework.http.HttpMethod
+import org.springframework.http.ResponseEntity
 
 class ProductsTests : AbstractApplicationTest() {
     @Test
-    fun `test create product`() {
+    fun testProduct() {
         val productDTO = ProductDTO("ean", "sku", "name", "brand", "category", 1.0f)
 
         restTemplate.postForLocation("http://localhost:$port/API/products", productDTO)
 
-        val retrievedProduct = restTemplate.getForObject("http://localhost:$port/API/products/ean", ProductDTO::class.java)
+        val retrievedProduct = restTemplate.getForObject("http://localhost:$port/API/products/ean",ProductDTO::class.java)
 
         Assertions.assertEquals(productDTO, retrievedProduct)
+    }
+
+    @Test
+    fun testGetProduct() {
+        val productDTO = ProductDTO("ean", "sku", "name", "brand", "category", 1.0f)
+        val productDTOsecond = ProductDTO("ean1", "sku", "name", "brand", "category", 1.0f)
+        val productDTOthird = ProductDTO("ean2", "sku", "name", "brand", "category", 1.0f)
+
+        restTemplate.postForLocation("http://localhost:$port/API/products", productDTO)
+        restTemplate.postForLocation("http://localhost:$port/API/products", productDTOsecond)
+        restTemplate.postForLocation("http://localhost:$port/API/products", productDTOthird)
+
+        val rateResponse: ResponseEntity<List<ProductDTO>> = restTemplate.exchange("http://localhost:$port/API/products",
+            HttpMethod.GET, null, object : ParameterizedTypeReference<List<ProductDTO>>() {})
+        val rates: List<ProductDTO>? = rateResponse.getBody()
+
+        if (rates != null) {
+            Assertions.assertEquals(rates.size, 3)
+        }
     }
 }
