d4591cf8ae398a06350096f96affa2a08f90e9b8
Roberto Caschetto <roberto.caschetto@gmail.com>
2023-05-12 22:12:29 +0200

    add jib dependencies

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -3,10 +3,13 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "3.0.5"
     id("io.spring.dependency-management") version "1.1.0"
+    id ("com.google.cloud.tools.jib") version "3.3.1"
     kotlin("jvm") version "1.7.22"
     kotlin("plugin.spring") version "1.7.22"
     kotlin("plugin.jpa") version "1.7.22"
 }
+
+jib.to.image = "warocky/webapp2g04"
 
 group = "it.polito.wa2"
 version = "0.0.1-SNAPSHOT"
