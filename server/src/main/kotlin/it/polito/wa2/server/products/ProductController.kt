23496589f90a6cf619e6fe2906b13540ea49e050
Edoardo Alberti <edoalberti99@gmail.com>
2023-05-25 17:31:58 +0200

    feat(keycloak): working keycloak??

--- server/src/main/kotlin/it/polito/wa2/server/products/ProductController.kt
+++ server/src/main/kotlin/it/polito/wa2/server/products/ProductController.kt
@@ -1,8 +1,14 @@
 package it.polito.wa2.server.products
 
 import it.polito.wa2.server.NotValidException
+import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
+import org.springframework.security.access.annotation.Secured
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User
 import org.springframework.web.bind.annotation.*
+import java.security.Principal
 
 @RestController
 @RequestMapping("/api")
@@ -17,10 +23,18 @@ class ProductController(
         return productService.addProduct(productDTO)
     }
 
+    /*
     @GetMapping("/products")
     @ResponseStatus(HttpStatus.OK)
     fun getAll(): List<ProductDTO> {
         return productService.getAll()
+    }
+    */
+
+    @GetMapping("/products")
+    @ResponseStatus(HttpStatus.OK)
+    fun getAll(@AuthenticationPrincipal user: DefaultOAuth2User?): String {
+        return user?.name ?: "null"
     }
 
     @GetMapping("/products/{ean}")
