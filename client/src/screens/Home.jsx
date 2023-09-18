28a25a2da38d79ece5cd8052000988a785009796
Edoardo Alberti <edoalberti99@gmail.com>
2023-09-18 16:09:17 +0200

    home login

--- client/src/screens/Home.jsx
+++ client/src/screens/Home.jsx
@@ -1,3 +1,30 @@
+import { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
 export default function Home() {
-   return <div>home</div>
+   const navigate = useNavigate()
+
+   useEffect(() => {
+      const fun = async () => {
+         const res = await fetch('/api/user', { method: 'GET' })
+         if (!res.ok) return
+         const user = await res.json()
+         if (user.role === 'NO_AUTH') return
+         localStorage.setItem('user', JSON.stringify(user))
+         navigate('/dashboard')
+      }
+      fun()
+   }, [])
+
+   return (
+      <div>
+         <button
+            type="button"
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+            onClick={() => (window.location.href = 'http://localhost:8081/login')}
+         >
+            Login
+         </button>
+      </div>
+   )
 }
