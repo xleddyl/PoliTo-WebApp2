5856a8b4fab0da10e577e7f924480555708fcf9a
Edoardo Alberti <edoalberti99@gmail.com>
2023-09-18 16:11:12 +0200

    register

--- client/src/screens/Home.jsx
+++ client/src/screens/Home.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 export default function Home() {
@@ -24,6 +24,13 @@ export default function Home() {
             onClick={() => (window.location.href = 'http://localhost:8081/login')}
          >
             Login
+         </button>
+         <button
+            type="button"
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+            onClick={() => navigate('/signup')}
+         >
+            Signup
          </button>
       </div>
    )
